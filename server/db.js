1584f08ff0edd9ad749a05f79f721b91a6cab855
CodingJoseph <josephpeery4business@gmail.com>
2025-08-15 17:33:11 -0400

    database connectivity set up (test)

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:ddSyBWBTEqc6imX1vJEGiRD9@172.21.248.167:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
