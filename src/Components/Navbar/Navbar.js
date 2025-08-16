48b8d3001f0b4a1620b7b344ac18e830cec895f6
CodingJoseph <josephpeery4business@gmail.com>
2025-08-16 04:10:51 -0400

    established backend connectivity for SignUp page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 // Necessary modules from React library.
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 // Doctor avatar logo for image src.
 import logo from './doctor_avatar.png';
 // Custom styles.
@@ -7,6 +8,46 @@ import './Navbar.css';
 
 // Function component for Navbar.
 const Navbar = () => {
+    // useState hooks for states.
+    const [click, setClick] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const [email , setEmail] = useState("");
+    const [showDropdown, setShowDropdown] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    const handleLogout = () => {
+        // Removes all session storage and local storage used for logging in.
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        // Removes email phone.
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        setUsername("");
+        // Removes the reviewFormData from local storage.
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if (key.startsWith("reviewFormData_")) {
+                localStorage.removeItem(key);
+            }
+            }
+            setEmail('');
+            window.location.reload();
+    };
+    
+    useEffect(() => {
+        const storedEmail = sessionStorage.getItem("email");
+        if (storedEmail) {
+            setIsLoggedIn(true);
+            setUsername(storedEmail);
+        }
+    }, []);
+
+    const handleDropdown = () => { setShowDropdown(!showDropdown) };
+
+    // JSX to render Navbar element.
     return (
         <nav>
         {/* Navigation logo section */}
@@ -15,28 +56,32 @@ const Navbar = () => {
             <a href="/">StayHealthy <img className="nav-logo-img" src={logo} /></a>
         </div>
         {/* Navigation icon section with an onClick event listener */}
-        <div className="nav-icon" onClick="{handleClick}">
+        <div className="nav-icon" onClick={handleClick}>
             {/* Hamburger Menu (called from Bootstrap Font Awesome) */}
-            <i className="fa fa-times fa fa-bars" />
+            <i className={click ? "fa fa-times" : "fa fa-bars"} />
         </div>
         {/* Unordered list for navigation links with 'active' class */}
-        <ul className="nav-links active">
+        <ul className={click ? "nav-links active" : "nav-links"}>
             <li className="link">
-            <a href="/">Home</a>
+                <Link to="/">Home</Link>
             </li>
             <li className="link">
-            <a href="#">Appointments</a>
+                <Link to="/search/doctors">Appointments</Link>
             </li>
-            <li className="link">
-            <a href="/signup">
-                <button className="btn1">Sign Up</button>
-            </a>
-            </li>
-            <li className="link">
-            <a href="/login">
-                <button className="btn1">Login</button>
-            </a>
-            </li>
+            {isLoggedIn?(
+                <>
+                    <li className="link">
+                        <button className="btn2" onClick={handleLogout}>Logout</button>
+                    </li>
+                </> ) : ( <>
+                    <li className="link">
+                        <Link to="/signup"><button className="btn1">Sign Up</button></Link>
+                    </li>
+                    <li className="link">
+                        <Link to="/login"><button className="btn1">Login</button></Link>
+                    </li>
+                </>
+            )}
         </ul>
         </nav>
     );
--- src/Components/Sign_Up/SignUp.js
+++ src/Components/Sign_Up/SignUp.js
@@ -1,15 +1,64 @@
-
+// Necessary modules from React library.
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 // Custom styles.
 import './SignUp.css';
 
 // Function component for SignUp.
 const Sign_Up = () => {
+    // useState hooks for states.
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    // State to show error messages.
+    const [showerr, setShowerr] = useState('');
+    // Navigation hook for react-router.
+    const navigate = useNavigate(); 
+
+    // Handles form submission.
+    const register = async (e) => {
+        // Prevents default form submission.
+        e.preventDefault();
+
+        // Registers user via API call (fetch request).
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+                phone: phone
+            })
+        });
+        // Parses response into JSON.
+        const json = await response.json();
+        if (json.authtoken) {
+            // Stores user data in session storage.
+            sessionStorage.setItem("auth-token", json.authtoken);
+            sessionStorage.setItem("name", name);
+            sessionStorage.setItem("phone", phone);
+            sessionStorage.setItem("email", email);
+            // Redirects user to home page and refreshes page.
+            navigate("/");
+            window.location.reload();
+        } else {
+            if (json.error) {
+                setShowerr(json.error);
+                console.log(json.error);
+            }
+        }
+    };
+
+    // JSX to render SignUp page.
     return (
         <div className="signup-container">
         <h1>Sign Up</h1>
-        <h3>Already a member? <a href="/login">Login</a></h3>
+        <h3>Already a member? <Link to="/login">Login</Link></h3>
         {/* Form that REQUIRES Role, Name, Phone No., Email, and Password. Submit and Reset button at the end. */}
-        <form className="signup-form">
+        <form method="POST" onSubmit={register} className="signup-form" autoComplete="off">
             <table>
             <tbody>
                 <tr>
@@ -25,19 +74,19 @@ const Sign_Up = () => {
                 </tr>
                 <tr>
                     <td><label htmlFor="name">Name</label></td>
-                    <td><input type="text" name="name" id="name" required placeholder="Your name here" /></td>
+                    <td><input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" required={true} placeholder="Your name here" minLength="4" aria-describedby="helpId" /></td>
                 </tr>
                 <tr>
                     <td><label htmlFor="phone">Phone #</label></td>
-                    <td><input type="tel" name="phone" id="phone" required placeholder="Your phone number here" maxLength="10" /></td>
+                    <td><input value={phone} onChange={(e) => setPhone(e.target.value)} type="tel" name="phone" id="phone" required={true} placeholder="Your phone number here" maxLength="10" aria-describedby="helpId" /></td>
                 </tr>
                 <tr>
                     <td><label htmlFor="email">E-mail</label></td>
-                    <td><input type="email" name="email" id="email" required placeholder="Your e-mail address here" /></td>
+                    <td><input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" required={true} placeholder="Your e-mail address here" aria-describedby="helpId" /></td>
                 </tr>
                 <tr>
                     <td><label htmlFor="password">Password</label></td>
-                    <td><input name="password" id="password" required placeholder="Your password here" /></td>
+                    <td><input value={password} onChange={(e) => setPassword(e.target.value)} type="text" name="password" id="password" required={true} placeholder="Your password here" minLength="8" aria-describedby="helpId" /></td>
                 </tr>
             </tbody>
             </table>
@@ -45,6 +94,7 @@ const Sign_Up = () => {
                 <button type="submit" className="btn1">Submit</button>
                 <button type="reset" className="btn1">Reset</button>
             </div>
+            {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
         </form>
         </div>
     );
